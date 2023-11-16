acc136c61f4c9e118853b801373030d59e011f5a
Behnam Banaei <behban20@gmail.com>
2023-11-16 10:20:44 +0330

    add second activity for developer panel with launcher action in manifest

--- app/src/main/java/com/behnawwm/developerpanelplayground/DeveloperPanelActivity.kt
+++ app/src/main/java/com/behnawwm/developerpanelplayground/DeveloperPanelActivity.kt
@@ -12,12 +12,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.behnawwm.developerpanelplayground.ui.theme.DeveloperPanelPlaygroundTheme
 
-class MainActivity : ComponentActivity() {
+class DeveloperPanelActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DeveloperPanelPlaygroundTheme {
-                Text(text = "main activity")
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text = "developer panel")
+                }
             }
         }
     }
